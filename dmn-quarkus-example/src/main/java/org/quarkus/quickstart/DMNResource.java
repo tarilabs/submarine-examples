0c83f1be1583e0a9a433b7d904ed441f4e7a6c74
tarilabs <matteo.mortari@gmail.com>
2019-04-04 14:10:51 +0200

    WIP

--- dmn-quarkus-example/src/main/java/org/quarkus/quickstart/DMNResource.java
+++ dmn-quarkus-example/src/main/java/org/quarkus/quickstart/DMNResource.java
@@ -1,7 +1,12 @@
 package org.quarkus.quickstart;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,31 +15,45 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.drools.core.RuleBaseConfiguration;
+import org.drools.core.definitions.InternalKnowledgePackage;
+import org.drools.core.definitions.ResourceTypePackageRegistry;
+import org.drools.core.definitions.impl.KnowledgePackageImpl;
 import org.drools.core.impl.KnowledgeBaseImpl;
+import org.kie.api.io.ResourceType;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNMessage;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNResult;
 import org.kie.dmn.api.core.DMNRuntime;
 import org.kie.dmn.core.compiler.DMNCompilerImpl;
+import org.kie.dmn.core.impl.DMNPackageImpl;
 import org.kie.dmn.core.impl.DMNRuntimeImpl;
 
 @Path("/dmn")
 public class DMNResource {
 
-    static final DMNRuntime dmnRuntime = new DMNRuntimeImpl(new KnowledgeBaseImpl("", new RuleBaseConfiguration()));
-    static final DMNModel dmnModel;
+    static final DMNRuntime dmnRuntime;
     static {
-        InputStream is = DMNResource.class.getResourceAsStream("/greetings.dmn");
+        KnowledgeBaseImpl knowledgeBase = new KnowledgeBaseImpl("", new RuleBaseConfiguration());
+        Map<String, InternalKnowledgePackage> pkgs = knowledgeBase.getPackagesMap();
         DMNCompilerImpl compilerImpl = new DMNCompilerImpl();
-        dmnModel = compilerImpl.compile(new InputStreamReader(is));
+        try {
+            List<java.nio.file.Path> files = Files.walk(Paths.get("."))
+                                                  .filter(path -> Files.isRegularFile(path) && path.toString().endsWith(".dmn"))
+                                                  .peek(System.out::println)
+                                                  .collect(Collectors.toList());
+            for (java.nio.file.Path file : files) {
+                DMNModel m = compilerImpl.compile(new FileReader(file.toFile()));
+                InternalKnowledgePackage pkg = pkgs.computeIfAbsent(m.getNamespace(), ns -> new KnowledgePackageImpl(ns));
+                ResourceTypePackageRegistry rpkg = pkg.getResourceTypePackages();
+                DMNPackageImpl dmnpkg = rpkg.computeIfAbsent(ResourceType.DMN, rtp -> new DMNPackageImpl(m.getNamespace()));
+                dmnpkg.addModel(m.getName(), m);// TODO add profiles?
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dmnRuntime = new DMNRuntimeImpl(knowledgeBase);
     }
-    //    static final DMNModel dmnModel2;
-    //    static {
-    //        InputStream is = DMNResource.class.getResourceAsStream("/adult.dmn");
-    //        DMNCompilerImpl compilerImpl = new DMNCompilerImpl();
-    //        dmnModel2 = compilerImpl.compile(new InputStreamReader(is));
-    //    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -42,23 +61,11 @@ public class DMNResource {
     public String dmn(@PathParam("name") String name) {
         DMNContext ctx = dmnRuntime.newContext();
         ctx.set("a name", name);
-        DMNResult evaluateAll = dmnRuntime.evaluateAll(dmnModel, ctx);
+        DMNResult evaluateAll = dmnRuntime.evaluateAll(dmnRuntime.getModels().get(0), ctx);
         for (DMNMessage m : evaluateAll.getMessages()) {
             System.out.println(m);
         }
         return evaluateAll.toString();
     }
 
-    //    @GET
-    //    @Produces(MediaType.TEXT_PLAIN)
-    //    @Path("/dmn2/{name}")
-    //    public String dmn2(@PathParam("name") String name) {
-    //        DMNContext ctx = dmnRuntime.newContext();
-    //        ctx.set("a number", Long.valueOf(name));
-    //        DMNResult evaluateAll = dmnRuntime.evaluateAll(dmnModel2, ctx);
-    //        for (DMNMessage m : evaluateAll.getMessages()) {
-    //            System.out.println(m);
-    //        }
-    //        return evaluateAll.toString();
-    //    }
 }
